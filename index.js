272cee064f1842c43cc9c317a5e8103891ce2bfe
SangeethaShanmugm <sanjuriya22@gmail.com>
2024-01-27 19:20:04 +0530

    updated

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())//Inbuilt Middleware
-const MONGO_URL = 'mongodb://127.0.0.1:27017';
+const MONGO_URL = 'mongodb://127.0.0.1:27017/ed-sep-intern';
 // 'mongodb://localhost:27017';
 let db;
 //http => get, post, put, delete
